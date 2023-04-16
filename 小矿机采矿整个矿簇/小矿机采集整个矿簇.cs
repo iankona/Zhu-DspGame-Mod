fe025f552f919bd3290128a528fb2c695603a997
iankona <419204845@qq.com>
2023-04-16 13:14:47 +0800

    添加项目-小矿机采矿整个矿簇
    功能是小采矿机正常放，但它会采集整个矿簇（1个矿组里有多条矿脉），小采矿机只要碰到矿组中任意一个矿脉就可以采集整个矿组并满黄带输出

---
+++ 小矿机采矿整个矿簇/小矿机采集整个矿簇.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+namespace 小矿机采集整个矿簇
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess(GAME_PROCESS)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "cn.zhufile.dsp.miningveingroup";
+        public const string NAME = "小矿机采集整个矿簇";
+        public const string VERSION = "1.0.0";
+        private const string GAME_PROCESS = "DSPGAME.exe";
+
+
+        public void Start()
+        {
+            new Harmony(GUID).PatchAll(typeof(小矿机采集整个矿簇));
+        }
+    }
+
+
+    [HarmonyPatch(typeof(MinerComponent), "ArrangeVeinArray")]
+    class 小矿机采集整个矿簇
+    {
+        public static void Postfix(ref MinerComponent __instance)
+        {
+            int entityid = __instance.entityId;
+            EntityData entity = GameMain.mainPlayer.factory.entityPool[entityid];
+            if (entity.protoId != 2301)
+            {
+                return;
+            }
+            int groupindex = GameMain.mainPlayer.factory.veinPool[__instance.veins[0]].groupIndex;
+            VeinGroup group = GameMain.mainPlayer.factory.veinGroups[groupindex];
+            __instance.veins = new int[group.count];
+            __instance.veinCount = group.count;
+            __instance.currentVeinIndex = group.count-1;
+
+            int currentindex = -1;
+            foreach (var vein in GameMain.mainPlayer.factory.veinPool)
+            {
+                if (vein.groupIndex == groupindex)
+                {
+                    ++currentindex;
+                    __instance.veins[currentindex] = vein.id;  
+                }
+            }
+
+        }
+    }
+}
+
+
