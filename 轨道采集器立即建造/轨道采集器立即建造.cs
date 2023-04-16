27cf721e62d9573915d91e66d6ac2c438b560a6b
iankona <419204845@qq.com>
2023-04-16 13:09:57 +0800

    添加项目-轨道采集器立即建造
    功能与ZHUInstantBuild.dll相同，并将范围限定在轨道采集器，对其他建筑不起作用。ZHU轨道采集器立即建造.dll与ZHUInstantBuild.dll冲突，2者只能选用1个dll放到BepInEX的plugins文件夹里

---
+++ 轨道采集器立即建造/轨道采集器立即建造.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+
+
+namespace 轨道采集器立即建造
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess(GAME_PROCESS)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "cn.zhufile.dsp.gasminerinstantbuild";
+        public const string NAME = "轨道采集器立即建造";
+        public const string VERSION = "1.0.0";
+        private const string GAME_PROCESS = "DSPGAME.exe";
+
+
+        public void Awake()
+        {
+            new Harmony(GUID).PatchAll(typeof(轨道采集器立即建造));
+        }
+    }
+
+
+    [HarmonyPatch(typeof(MechaDroneLogic), "UpdateTargets")]
+    class 轨道采集器立即建造
+    {
+        public static void Postfix(MechaDroneLogic __instance)
+        {
+            foreach (var prebuild in __instance.factory.prebuildPool)
+            {
+                if (prebuild.protoId == 2105) // __instance.planet.type == EPlanetType.Gas
+                {
+                    __instance.factory.BuildFinally(__instance.player, prebuild.id);
+                }
+                    
+            }
+                
+        }
+    }
+}
+
