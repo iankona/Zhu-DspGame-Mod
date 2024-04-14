8403367abf03884b62a3cde2ffb836b396a61793
iankona <419204845@qq.com>
2024-04-14 17:50:38 +0800

    Zhu星系内快速移动.dll添加建造模式新增右键快速移动功能
    修改游戏本体的rtsCancel操作模式，在保留原本的右键快速取消功能的同时新增右键按住快速移动/快速旅行功能，极大的改善建造模式需要频繁跑动打断思路的缺点，现在可以专注于建造本身。

--- 星系内快速移动/星系内快速移动.cs
+++ 星系内快速移动/星系内快速移动.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using BepInEx;
 using HarmonyLib;
+using UnityEngine;
+using static UnityEngine.RectTransform;
+using static VFInput;
 
 namespace 星系内快速移动
 {
@@ -39,6 +42,8 @@ namespace 星系内快速移动
     class 全球视图启用沙盒工具
     {
         public static bool sandboxToolsEnabled;
+        public static bool skiprtscancel = false;
+
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(UIGlobemap), "_OnUpdate")]
@@ -56,6 +61,57 @@ namespace 星系内快速移动
         {
             Traverse.Create(GameMain.instance).Field("_sandboxToolsEnabled").SetValue(sandboxToolsEnabled);
         }
+
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(UIBuildMenu), "UpdateUXPanelHotKey")]
+        public static void 函数03(UIBuildMenu __instance) 
+        {
+            UIGame uiGame = UIRoot.instance.uiGame;
+            if (UIGame.viewMode == EViewMode.Build)
+                uiGame.globemap.TeleportLogic();
+            if (__instance.isDismantleMode || __instance.isUpgradeMode)
+                skiprtscancel = true;
+            else
+            { 
+                if (Input.GetMouseButtonUp(0))
+                    skiprtscancel = false;
+            }
+            if (Input.GetMouseButtonUp(1))
+                skiprtscancel = true;
+
+
+
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(VFInput), "rtsCancel", MethodType.Getter)]
+        public static bool 函数04()
+        {
+
+            if (skiprtscancel)
+            {
+                VFInput.InputValue inputValue = new VFInput.InputValue();
+                return inputValue;
+            }
+            else
+            {
+                VFInput.InputValue inputValue = new VFInput.InputValue();
+                VFInput.InputAxis axis = VFInput.axis_button;
+                int axisIdx = 1;
+                if (axisIdx >= 0 && axisIdx < axis.value.Length)
+                {
+                    inputValue.pressing = (double)axis.value[axisIdx] > 0.5;
+                    inputValue.onDown = axis.down[axisIdx];
+                    inputValue.onUp = axis.up[axisIdx];
+                    inputValue.value = axis.value[axisIdx];
+                }
+                return inputValue;
+            }
+
+        }
+
     }
 
 
@@ -100,23 +156,23 @@ namespace 星系内快速移动
         }
 
 
-        //// 启用星系团右键快速移动功能
-        //[HarmonyPrefix]
-        //[HarmonyPatch(typeof(UIStarmap), "OnScreenClick")]
-        //public static bool 函数21(UIStarmap __instance)
-        //{
-        //    sandboxToolsEnabled = (bool)Traverse.Create(GameMain.instance).Field("_sandboxToolsEnabled").GetValue();
-        //    if (!sandboxToolsEnabled)
-        //        Traverse.Create(GameMain.instance).Field("_sandboxToolsEnabled").SetValue(true);
-        //    return true;
-        //}
+        // 启用星系团右键快速移动功能
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UIStarmap), "OnScreenClick")]
+        public static bool 函数21(UIStarmap __instance)
+        {
+            sandboxToolsEnabled = (bool)Traverse.Create(GameMain.instance).Field("_sandboxToolsEnabled").GetValue();
+            if (!sandboxToolsEnabled)
+                Traverse.Create(GameMain.instance).Field("_sandboxToolsEnabled").SetValue(true);
+            return true;
+        }
 
-        //[HarmonyPostfix]
-        //[HarmonyPatch(typeof(UIStarmap), "OnScreenClick")]
-        //public static void 函数22(UIStarmap __instance)
-        //{
-        //    Traverse.Create(GameMain.instance).Field("_sandboxToolsEnabled").SetValue(sandboxToolsEnabled);
-        //}
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(UIStarmap), "OnScreenClick")]
+        public static void 函数22(UIStarmap __instance)
+        {
+            Traverse.Create(GameMain.instance).Field("_sandboxToolsEnabled").SetValue(sandboxToolsEnabled);
+        }
 
     }
 }
