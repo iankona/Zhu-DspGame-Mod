5e67137d49536db09320474a7e4cc73b5d977750
iankona <419204845@qq.com>
2024-04-15 12:39:29 +0800

    增加小采矿机的采矿距离
    小采矿机的采矿范围由7.5f增加到18.0f，配合采集整个矿族，使得围绕矿点四方方正布局成为可能。

--- 小矿机采集整个矿簇/小矿机采集整个矿簇.cs
+++ 小矿机采集整个矿簇/小矿机采集整个矿簇.cs
@@ -74,6 +74,60 @@ namespace 小矿机采集整个矿簇
             return true;
         }
 
+
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(NearColliderLogic), "GetVeinsInAreaNonAlloc")]
+        public static bool 函数2(NearColliderLogic __instance, ref Vector3 center, ref float areaRadius, ref int[] veinIds)
+        {
+            areaRadius = 18.0f; // 10 -> 18
+            return true;
+        }
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MinerComponent), "IsTargetVeinInRange")] // 静态方法, 超过扇形范围返回false
+        public static void 函数3(Vector3 vPos, Pose lPose, PrefabDesc desc, ref bool __result)
+        {
+            __result = true;
+            Vector3 forward = lPose.forward;
+            if (desc.veinMiner)
+            {
+                if (desc.isVeinCollector)
+                {
+                    Vector3 vector3 = lPose.position + forward * -10f;
+                    Vector3 rhs = -forward;
+                    Vector3 right = lPose.right;
+                    Vector3 lhs = vPos - vector3;
+                    double sqrMagnitude = (double)lhs.sqrMagnitude;
+                    float num1 = Mathf.Abs(Vector3.Dot(lhs, rhs));
+                    float num2 = Mathf.Abs(Vector3.Dot(lhs, right));
+                    if (sqrMagnitude > 100.0 || (double)num1 > 7.75 || (double)num2 > 6.25) //  
+                        __result = false;
+
+                }
+                else
+                {
+                    Vector3 vector3_1 = lPose.position + forward * -1.2f;
+                    Vector3 rhs1 = -forward;
+                    Vector3 up = lPose.up;
+                    Vector3 rhs2 = vPos - vector3_1;
+                    float f = Vector3.Dot(up, rhs2);
+                    Vector3 vector3_2 = rhs2 - up * f;
+                    double sqrMagnitude = (double)vector3_2.sqrMagnitude;
+                    float num = Vector3.Dot(vector3_2.normalized, rhs1);
+                    // 小采矿机的判断代码
+                    if ( (double)num < 0.73000001907348633 || (double)Mathf.Abs(f) > 2.0) // sqrMagnitude > 961.0 / 16.0 ||
+                        __result = false;
+                }
+            }
+
+        }
+
+
+
+
+
     }
  
 }
